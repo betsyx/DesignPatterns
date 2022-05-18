5540aee5c370d4925036553d4914ad8bc0f0175c
icefire <2295667871@qq.com>
2022-05-18 14:02:41 +0800

    icefire storehouse init

---
+++ src/main/java/com/icefire/keep/move/designpatterns/template/vo/TemplateMethodPattern.java
@@ -0,0 +1,29 @@
+package com.icefire.keep.move.designpatterns.template.vo;
+
+import lombok.Data;
+
+/**
+ * @author: icefire
+ * @date: 2022/5/18 11:29
+ * @description:
+ */
+@Data
+public class TemplateMethodPattern {
+
+    public void TemplateMethd(){
+
+    }
+
+    public void getUp(){
+
+    }
+
+    public void eat(){
+
+    }
+
+    public void practice(){
+
+    }
+
+}
